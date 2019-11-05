78b9bcf4305b12ec5915569cff5ade96d1652618
SharathNaik <nayak.sharathm@gmail.com>
2019-11-05 09:50:27 +0530

    all files added

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const AboutPage = () => (
+  <div>
+    <h2>About</h2>
+    <p>This App uses React,Redux, React Router and useful libraries</p>
+  </div>
+);
+
+export default AboutPage;
