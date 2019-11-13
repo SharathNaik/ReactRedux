3dcb7d9d3cd669e4fd217085265775de1ef415b2
M <sharath.m@capgemini.com>
2019-11-13 19:21:45 +0530

    Testing React - Completed.

---
+++ src/components/courses/CourseForm.Snapshots.test.js
@@ -0,0 +1,41 @@
+import React from "react";
+import CourseForm from "./CourseForm";
+import renderer from "react-test-renderer";
+import { courses, authors } from "../../../tools/mockData";
+import { isTaggedTemplateExpression, exportAllDeclaration } from "../../../node_modules/@babel/types";
+import { JestEnvironment } from "../../../node_modules/@jest/environment";
+
+
+it("sets submit button label 'saving...' when saving is true", () => {
+
+    const tree = renderer.create(
+        <CourseForm
+            course={courses[0]}
+            authors={authors}
+            onSave={jest.fn()}
+            onChange={jest.fn()}
+            saving
+        />
+    );
+
+    expect(tree).toMatchSnapshot();
+
+});
+
+
+
+it("sets submit button label 'save' when saving is false", () => {
+
+    const tree = renderer.create(
+        <CourseForm
+            course={courses[0]}
+            authors={authors}
+            onSave={jest.fn()}
+            onChange={jest.fn()}
+            saving={false}
+        />
+    );
+
+    expect(tree).toMatchSnapshot();
+
+});
